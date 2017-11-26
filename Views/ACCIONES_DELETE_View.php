68f98505f589492e028481e6631eab9950ecdf2a
AlexVC14 <alexvolocid@gmail.com>
2017-11-26 19:32:49 +0100

    no message

--- Views/ACCIONES_DELETE_View.php
+++ Views/ACCIONES_DELETE_View.php
@@ -54,9 +54,9 @@ class ACCIONES_DELETE {
 				<?php echo $strings['¿Está seguro de que quiere borrar esta tupla de la tabla?'];?>
 			</p>
 			<form action="../Controllers/USUARIOS_CONTROLLER.php" method="post" style="display: inline">
-				<input type="hidden" name="login" value=<?php echo $this->valores['idgroup'] ?> />
-				<input type="hidden" name="nombre" value=<?php echo $this->valores['nombreG'] ?> />
-				<input type="hidden" name="apellidos" value=<?php echo $this->valores['descrip'] ?> />
+				<input type="hidden" name="login" value=<?php echo $this->valores['idaccion'] ?> />
+				<input type="hidden" name="nombre" value=<?php echo $this->valores['nombreAc'] ?> />
+				<input type="hidden" name="apellidos" value=<?php echo $this->valores['descripAc'] ?> />
 				<input id="DELETE" name="action" value="DELETE" type="image" src="../Views/icon/confirmar.png" width="32" height="32" alt="<?php echo $strings['Confirmar'] ?>">
 			</form>
 			<form action='../Controllers/USUARIOS_CONTROLLER.php' method="post" style="display: inline">
