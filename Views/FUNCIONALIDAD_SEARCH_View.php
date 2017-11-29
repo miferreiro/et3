2d14adb258165a88f2d3de0fbeb97691ea4f9e6b
AlexVC14 <alexvolocid@gmail.com>
2017-11-29 12:07:23 +0100

    no message

--- Views/FUNCIONALIDAD_SEARCH_View.php
+++ Views/FUNCIONALIDAD_SEARCH_View.php
@@ -36,7 +36,7 @@ class FUNCIONALIDAD_SEARCH {
 					</tr>
 					<tr>
 						<th class="formThTd">
-							<?php echo $strings['DescripcFuncionalidad'];?>
+							<?php echo $strings['DescripFuncionalidad'];?>
 						</th>
 						<td class="formThTd"><textarea id="DescripFuncionalidad" name="DescripFuncionalidad" placeholder="<?php echo $strings['Escriba aqui...']?>" cols="50" rows="3" value="" maxlength="100"  onBlur="comprobarLongitud(this,'100') && comprobarTexto(this,'100') && comprobarAlfabetico(this,'100')"/></textarea>
 					</tr>
