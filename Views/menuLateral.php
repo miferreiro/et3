5f82e04640eac90dd1674a755b67fa63d0cb3d3c
Jony <jonycr717@gmail.com>
2017-11-29 16:27:48 +0100

    Retoque menu lateral

--- Views/menuLateral.php
+++ Views/menuLateral.php
@@ -1,66 +1,77 @@
 <!--Creado el 8 de noviembre del 2017 por de3t7q/-->
 <!--Contiene la vista del menu lateral-->
     <script type="text/javascript" src="../Views/js/desplegarMenu.js"></script> 
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript">
-	</script>
-	<link href="../Views/css/estiloFinal.css" rel="stylesheet" type="text/css">
-
-		<div class="menu">
-			<ul>
-				<!--Diferentes opciones de la lista y sus links-->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
+	<link rel="stylesheet"  type="text/css" href="../Views/css/estilos.css" />
 
 
-				<li>
-					<div>
-						<!-- <a href="#cabecera"><?php echo $strings['Inicio']; ?></a> -->
-					</div>
-				</li>
-
-
-				<li>
-					<!-- <a href="javascript:void();"><?php echo $strings['usuarios']; ?></a> -->
-
-					<ul>
+<nav>
+		<ul class ="menu">
+			<li>
+				<a href="#" class="primerNivel"><?php echo $strings['Gestion de usuarios']; ?></a> 
+					<ul class="submenu">
 						<li>
-							<a href="../Controllers/USUARIO_CONTROLLER.php"><?php echo $strings['Gestion de usuarios']; ?></a>
+							<a href="../Controllers/USUARIO_CONTROLLER.php" class="segundoNivel"><?php echo $strings['Mostrar todos']; ?></a>
 						</li>
-
-
 						<li>
-							<a href="../Controllers/GRUPO_CONTROLLER.php"><?php echo $strings['Gestion de grupo']; ?></a>
+							<a href="../Controllers/USUARIO_CONTROLLER.php?action=SEARCH" class="segundoNivel"><?php echo $strings['Buscador']; ?></a>
 						</li>
-
-						<li>
-							<a href="../Controllers/USU_GRUPO_CONTROLLER.php"><?php echo $strings['Gestion de usuarios-grupo']; ?></a>
-						</li>
-
 					</ul>
-
-					
-				</li>
-
-				<li>
-                     
-					<ul>
+			</li>
+			<li>
+				<a href="#" class="primerNivel"><?php echo $strings['Gestion de grupo']; ?></a> 
+					<ul class="submenu">
 						<li>
-							<a href="../Controllers/FUNC_ACCION_CONTROLLER.php"><?php echo $strings['Gestion de funcionalidades/acciones']; ?></a>
+							<a href="../Controllers/GRUPO_CONTROLLER.php" class="segundoNivel"><?php echo $strings['Mostrar todos']; ?></a>
 						</li>
-												<li>
-						    <a href="../Controllers/FUNCIONALIDAD_CONTROLLER.php"><?php echo $strings['Gestion de funcionalidades']; ?></a>
+						<li>
+							<a href="../Controllers/GRUPO_CONTROLLER.php?action=SEARCH" class="segundoNivel"><?php echo $strings['Buscador']; ?></a>
 						</li>
-												<li>
-							<a href="../Controllers/FUNC_ACCION_CONTROLLER.php"><?php echo $strings['Gestion de acciones']; ?></a>
-							
-						</li>
-
 					</ul>
-				</li>
-
-
-
-				<li>
-					<!-- <a href="#footer"><?php echo $strings['Footer']; ?></a> -->
-				</li>
-			</ul>
-		</div>
-                
+			</li>
+			<li>
+				<a href="#" class="primerNivel"><?php echo $strings['Gestion de usuarios-grupo']; ?></a> 
+					<ul class="submenu">
+						<li>
+							<a href="../Controllers/USU_GRUPO_CONTROLLER.php" class="segundoNivel"><?php echo $strings['Mostrar todos']; ?></a>
+						</li>
+						<li>
+							<a href="../Controllers/USU_GRUPO_CONTROLLER.php?action=SEARCH" class="segundoNivel"><?php echo $strings['Buscador']; ?></a>
+						</li>
+					</ul>
+			</li>
+			<li>
+				<a href="#" class="primerNivel"><?php echo $strings['Gestion de funcionalidades/acciones']; ?></a> 
+					<ul class="submenu">
+						<li>
+							<a href="../Controllers/FUNC_ACCION_CONTROLLER.php" class="segundoNivel"><?php echo $strings['Mostrar todos']; ?></a>
+						</li>
+						<li>
+							<a href="../Controllers/FUNC_ACCION_CONTROLLER.php?action=SEARCH" class="segundoNivel"><?php echo $strings['Buscador']; ?></a>
+						</li>
+					</ul>
+			</li>
+			<li>
+				<a href="#" class="primerNivel"><?php echo $strings['Gestion de funcionalidades']; ?></a> 
+					<ul class="submenu">
+						<li>
+							<a href="../Controllers/FUNCIONALIDAD_CONTROLLER.php" class="segundoNivel"><?php echo $strings['Mostrar todos']; ?></a>
+						</li>
+						<li>
+							<a href="../Controllers/FUNCIONALIDAD_CONTROLLER.php?action=SEARCH" class="segundoNivel"><?php echo $strings['Buscador']; ?></a>
+						</li>
+					</ul>
+			</li>
+			<li>
+				<a href="#" class="primerNivel"><?php echo $strings['Gestion de aciones']; ?></a> 
+					<ul class="submenu">
+						<li>
+							<a href="../Controllers/FUNC_ACCION_CONTROLLER.php" class="segundoNivel"><?php echo $strings['Mostrar todos']; ?></a>
+						</li>
+						<li>
+							<a href="../Controllers/FUNC_ACCION_CONTROLLER.php?action=SEARCH" class="segundoNivel"><?php echo $strings['Buscador']; ?></a>
+						</li>
+					</ul>
+			</li>
+		</ul>
+	</nav>
