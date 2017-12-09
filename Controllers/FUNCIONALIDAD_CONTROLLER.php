53e82882082e0f96f57f01293d2235fa3a868732
miferreiro <miguel.ferreiro.diaz@gmail.com>
2017-12-10 00:18:14 +0100

    Arregalo problema de permisos en gestión de funcionalidad

--- Controllers/FUNCIONALIDAD_CONTROLLER.php
+++ Controllers/FUNCIONALIDAD_CONTROLLER.php
@@ -7,7 +7,7 @@
 	Función: controlador que realiza las FUNCIONALIDAD, recibidas de las vistas, necesarias para realizar altas, bajas, modificaciones y búsquedas.
 */
 
-session_start();//solicito trabajar con la sesión
+session_start(); //solicito trabajar con la sesión
 
 include '../Models/FUNCIONALIDAD_MODEL.php';
 include '../Models/USU_GRUPO_MODEL.php'; //incluye el contendio del modelo usuarios
@@ -20,22 +20,20 @@ include '../Views/FUNCIONALIDAD_SHOWCURRENT_View.php';
 include '../Views/DEFAULT_View.php'; //incluye la vista por defecto
 include '../Views/MESSAGE_View.php';
 
-function get_data_form(){
-	
-	
-	$IdFuncionalidad = $_REQUEST['IdFuncionalidad'];
-	$NombreFuncionalidad = $_REQUEST['NombreFuncionalidad'];
-	$DescripFuncionalidad = $_REQUEST['DescripFuncionalidad'];
-	$Acciones = null;
-	$action= $_REQUEST['action'];
-	
+function get_data_form() {
+
+
+	$IdFuncionalidad = $_REQUEST[ 'IdFuncionalidad' ];
+	$NombreFuncionalidad = $_REQUEST[ 'NombreFuncionalidad' ];
+	$DescripFuncionalidad = $_REQUEST[ 'DescripFuncionalidad' ];
+	$action = $_REQUEST[ 'action' ];
+
 	$FUNCIONALIDAD = new FUNCIONALIDAD(
 		$IdFuncionalidad,
 		$NombreFuncionalidad,
-		$DescripFuncionalidad,
-		$Acciones
+		$DescripFuncionalidad
 	);
-	
+
 	return $FUNCIONALIDAD;
 }
 
@@ -48,26 +46,26 @@ switch ( $_REQUEST[ 'action' ] ) {
 	case 'ADD':
 		if ( !$_POST ) {
 			//Crea una nueva vista del formulario añadir
-			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ],'');
+			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ], '' );
 			$ADMIN = $USUARIO->comprobarAdmin();
-			if($ADMIN == true){
+			if ( $ADMIN == true ) {
 				new FUNCIONALIDAD_ADD();
-			}else{
-            $cont=0;
-			$PERMISO = $USUARIO->comprobarPermisos();
-			while ( $fila = mysqli_fetch_array( $PERMISO ) ) {
-			if($fila['IdFuncionalidad']=='4'){
-				if($fila['IdAccion']=='0'){
-			    //Crea una vista add para ver la tupla
-			     $cont=$cont+1;
+			} else {
+				$cont = 0;
+				$PERMISO = $USUARIO->comprobarPermisos();
+				while ( $fila = mysqli_fetch_array( $PERMISO ) ) {
+					if ( $fila[ 'IdFuncionalidad' ] == '4' ) {
+						if ( $fila[ 'IdAccion' ] == '0' ) {
+							//Crea una vista add para ver la tupla
+							$cont = $cont + 1;
+						}
+					}
 				}
-			   }
-			}
-			if($cont==1){
-			new FUNCIONALIDAD_ADD();
-		}else{
-			new MESSAGE( 'El usuario no tiene los permisos necesarios', '../Controllers/FUNCIONALIDAD_CONTROLLER.php' );
-		}
+				if ( $cont == 1 ) {
+					new FUNCIONALIDAD_ADD();
+				} else {
+					new MESSAGE( 'El usuario no tiene los permisos necesarios', '../Controllers/FUNCIONALIDAD_CONTROLLER.php' );
+				}
 			}
 		} else {
 			$FUNCIONALIDAD = get_data_form();
@@ -78,32 +76,32 @@ switch ( $_REQUEST[ 'action' ] ) {
 	case 'DELETE':
 		if ( !$_POST ) {
 			//Crea una nueva vista del formulario borrar
-			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ],'');
+			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ], '' );
 			$ADMIN = $USUARIO->comprobarAdmin();
-			if($ADMIN == true){
-			$FUNCIONALIDAD = new FUNCIONALIDAD( $_REQUEST[ 'IdFuncionalidad' ], '', '','');
-			$valores = $FUNCIONALIDAD->RellenaDatos( $_REQUEST[ 'IdFuncionalidad' ]);
-			$dependencias = $FUNCIONALIDAD->dependencias( $_REQUEST[ 'IdFuncionalidad' ]);
-			new FUNCIONALIDAD_DELETE( $valores, $dependencias );
-			}else{
-            $cont=0;
-			$PERMISO = $USUARIO->comprobarPermisos();
-			while ( $fila = mysqli_fetch_array( $PERMISO ) ) {
-			if($fila['IdFuncionalidad']=='4'){
-				if($fila['IdAccion']=='1'){
-			    //Crea una vista add para ver la tupla
-			     $cont=$cont+1;
+			if ( $ADMIN == true ) {
+				$FUNCIONALIDAD = new FUNCIONALIDAD( $_REQUEST[ 'IdFuncionalidad' ], '', '' );
+				$valores = $FUNCIONALIDAD->RellenaDatos( $_REQUEST[ 'IdFuncionalidad' ] );
+				$dependencias = $FUNCIONALIDAD->dependencias( $_REQUEST[ 'IdFuncionalidad' ] );
+				new FUNCIONALIDAD_DELETE( $valores, $dependencias );
+			} else {
+				$cont = 0;
+				$PERMISO = $USUARIO->comprobarPermisos();
+				while ( $fila = mysqli_fetch_array( $PERMISO ) ) {
+					if ( $fila[ 'IdFuncionalidad' ] == '4' ) {
+						if ( $fila[ 'IdAccion' ] == '1' ) {
+							//Crea una vista add para ver la tupla
+							$cont = $cont + 1;
+						}
+					}
 				}
-			   }
-			}
-			if($cont==1){
-			$FUNCIONALIDAD = new FUNCIONALIDAD( $_REQUEST[ 'IdFuncionalidad' ], '', '','');
-			$valores = $FUNCIONALIDAD->RellenaDatos( $_REQUEST[ 'IdFuncionalidad' ]);
-			$dependencias = $FUNCIONALIDAD->dependencias( $_REQUEST[ 'IdFuncionalidad' ]);
-			new FUNCIONALIDAD_DELETE( $valores, $dependencias );
-		}else{
-			new MESSAGE( 'El usuario no tiene los permisos necesarios', '../Controllers/FUNCIONALIDAD_CONTROLLER.php' );
-		}
+				if ( $cont == 1 ) {
+					$FUNCIONALIDAD = new FUNCIONALIDAD( $_REQUEST[ 'IdFuncionalidad' ], '', '' );
+					$valores = $FUNCIONALIDAD->RellenaDatos( $_REQUEST[ 'IdFuncionalidad' ] );
+					$dependencias = $FUNCIONALIDAD->dependencias( $_REQUEST[ 'IdFuncionalidad' ] );
+					new FUNCIONALIDAD_DELETE( $valores, $dependencias );
+				} else {
+					new MESSAGE( 'El usuario no tiene los permisos necesarios', '../Controllers/FUNCIONALIDAD_CONTROLLER.php' );
+				}
 			}
 
 		} else {
@@ -114,144 +112,145 @@ switch ( $_REQUEST[ 'action' ] ) {
 		break;
 	case 'EDIT':
 		if ( !$_POST ) {
-						//Crea una nueva vista del formulario editar
-			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ],'');
+			//Crea una nueva vista del formulario editar
+			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ], '' );
 			$ADMIN = $USUARIO->comprobarAdmin();
-			if($ADMIN == true){
-			$FUNCIONALIDAD = new FUNCIONALIDAD( $_REQUEST[ 'IdFuncionalidad' ], '', '','');
-			$valores = $FUNCIONALIDAD->RellenaDatos( $_REQUEST[ 'IdFuncionalidad' ] );
-			new FUNCIONALIDAD_EDIT( $valores );
-			}else{
-            $cont=0;
-			$PERMISO = $USUARIO->comprobarPermisos();
-			while ( $fila = mysqli_fetch_array( $PERMISO ) ) {
-			if($fila['IdFuncionalidad']=='4'){
-				if($fila['IdAccion']=='2'){
-			    //Crea una vista add para ver la tupla
-			     $cont=$cont+1;
+			if ( $ADMIN == true ) {
+				$FUNCIONALIDAD = new FUNCIONALIDAD( $_REQUEST[ 'IdFuncionalidad' ], '', '' );
+				$valores = $FUNCIONALIDAD->RellenaDatos( $_REQUEST[ 'IdFuncionalidad' ] );
+				new FUNCIONALIDAD_EDIT( $valores );
+			} else {
+				$cont = 0;
+				$PERMISO = $USUARIO->comprobarPermisos();
+				while ( $fila = mysqli_fetch_array( $PERMISO ) ) {
+					if ( $fila[ 'IdFuncionalidad' ] == '4' ) {
+						if ( $fila[ 'IdAccion' ] == '2' ) {
+							//Crea una vista add para ver la tupla
+							$cont = $cont + 1;
+						}
+					}
 				}
-			   }
-			}
-			if($cont==1){
-			$FUNCIONALIDAD = new FUNCIONALIDAD( $_REQUEST[ 'IdFuncionalidad' ], '', '','');
-			$valores = $FUNCIONALIDAD->RellenaDatos( $_REQUEST[ 'IdFuncionalidad' ] );
-			new FUNCIONALIDAD_EDIT( $valores );
-		}else{
-			new MESSAGE( 'El usuario no tiene los permisos necesarios', '../Controllers/FUNCIONALIDAD_CONTROLLER.php' );
-		}
+				if ( $cont == 1 ) {
+					$FUNCIONALIDAD = new FUNCIONALIDAD( $_REQUEST[ 'IdFuncionalidad' ], '', '' );
+					$valores = $FUNCIONALIDAD->RellenaDatos( $_REQUEST[ 'IdFuncionalidad' ] );
+					new FUNCIONALIDAD_EDIT( $valores );
+				} else {
+					new MESSAGE( 'El usuario no tiene los permisos necesarios', '../Controllers/FUNCIONALIDAD_CONTROLLER.php' );
+				}
 			}
 
 		} else {
 			$FUNCIONALIDAD = get_data_form();
-			$respuesta = $FUNCIONALIDAD->EDIT($_REQUEST['IdAccion']);
+			$respuesta = $FUNCIONALIDAD->EDIT();
 			new MESSAGE( $respuesta, '../Controllers/FUNCIONALIDAD_CONTROLLER.php' );
 		}
 		break;
 	case 'SEARCH':
+		$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ], '' );
 		if ( !$_POST ) {
-			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ],'');
 			$ADMIN = $USUARIO->comprobarAdmin();
-			if($ADMIN == true){
+			if ( $ADMIN == true ) {
 				new FUNCIONALIDAD_SEARCH();
-			}else{
-            $cont=0;
-			$PERMISO = $USUARIO->comprobarPermisos();
-			while ( $fila = mysqli_fetch_array( $PERMISO ) ) {
-			if($fila['IdFuncionalidad']=='4'){
-				if($fila['IdAccion']=='3'){
-			    //Crea una vista add para ver la tupla
-			     $cont=$cont+1;
+			} else {
+				$cont = 0;
+				$PERMISO = $USUARIO->comprobarPermisos();
+				while ( $fila = mysqli_fetch_array( $PERMISO ) ) {
+					if ( $fila[ 'IdFuncionalidad' ] == '4' ) {
+						if ( $fila[ 'IdAccion' ] == '3' ) {
+							//Crea una vista add para ver la tupla
+							$cont = $cont + 1;
+						}
+					}
 				}
-			   }
-			}
-			if($cont==1){
-			new FUNCIONALIDAD_SEARCH();
-		}else{
-			new MESSAGE( 'El usuario no tiene los permisos necesarios', '../Controllers/FUNCIONALIDAD_CONTROLLER.php' );
-		}
+				if ( $cont == 1 ) {
+					new FUNCIONALIDAD_SEARCH();
+				} else {
+					new MESSAGE( 'El usuario no tiene los permisos necesarios', '../Controllers/FUNCIONALIDAD_CONTROLLER.php' );
+				}
 			}
 		} else {
+			$PERMISO = $USUARIO->comprobarPermisos();
 			$FUNCIONALIDAD = get_data_form();
 			$datos = $FUNCIONALIDAD->SEARCH();
-			$lista = array( 'IdFuncionalidad','NombreFuncionalidad','DescripFuncionalidad' );
-			new FUNCIONALIDAD_SHOWALL( $lista, $datos );
+			$lista = array( 'IdFuncionalidad', 'NombreFuncionalidad', 'DescripFuncionalidad' );
+			new FUNCIONALIDAD_SHOWALL( $lista, $datos,$PERMISO,true);
 		}
 		break;
-		
+
 	case 'SHOWCURRENT':
-			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ],'');
-			$ADMIN = $USUARIO->comprobarAdmin();
-			if($ADMIN == true){
-			$FUNCIONALIDAD= new FUNCIONALIDAD( $_REQUEST[ 'IdFuncionalidad' ], '', '','');
-		    $valores = $FUNCIONALIDAD->RellenaDatos( $_REQUEST[ 'IdFuncionalidad' ] );
-		    new FUNCIONALIDAD_SHOWCURRENT( $valores );
-			}else{
-            $cont=0;
+		$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ], '' );
+		$ADMIN = $USUARIO->comprobarAdmin();
+		if ( $ADMIN == true ) {
+			$FUNCIONALIDAD = new FUNCIONALIDAD( $_REQUEST[ 'IdFuncionalidad' ], '', '' );
+			$valores = $FUNCIONALIDAD->RellenaDatos( $_REQUEST[ 'IdFuncionalidad' ] );
+			new FUNCIONALIDAD_SHOWCURRENT( $valores );
+		} else {
+			$cont = 0;
 			$PERMISO = $USUARIO->comprobarPermisos();
 			while ( $fila = mysqli_fetch_array( $PERMISO ) ) {
-			if($fila['IdFuncionalidad']=='4'){
-				if($fila['IdAccion']=='4'){
-			    //Crea una vista add para ver la tupla
-			     $cont=$cont+1;
+				if ( $fila[ 'IdFuncionalidad' ] == '4' ) {
+					if ( $fila[ 'IdAccion' ] == '4' ) {
+						//Crea una vista add para ver la tupla
+						$cont = $cont + 1;
+					}
 				}
-			   }
 			}
-			if($cont==1){
-		    $FUNCIONALIDAD= new FUNCIONALIDAD( $_REQUEST[ 'IdFuncionalidad' ], '', '','');
-		    $valores = $FUNCIONALIDAD->RellenaDatos( $_REQUEST[ 'IdFuncionalidad' ] );
-		    new FUNCIONALIDAD_SHOWCURRENT( $valores );
-		}else{
-			new MESSAGE( 'El usuario no tiene los permisos necesarios', '../Controllers/FUNCIONALIDAD_CONTROLLER.php' );
+			if ( $cont == 1 ) {
+				$FUNCIONALIDAD = new FUNCIONALIDAD( $_REQUEST[ 'IdFuncionalidad' ], '', '' );
+				$valores = $FUNCIONALIDAD->RellenaDatos( $_REQUEST[ 'IdFuncionalidad' ] );
+				new FUNCIONALIDAD_SHOWCURRENT( $valores );
+			} else {
+				new MESSAGE( 'El usuario no tiene los permisos necesarios', '../Controllers/FUNCIONALIDAD_CONTROLLER.php' );
+			}
 		}
-			}
 		break;
 	default:
-		$USUARIO = new USU_GRUPO(  $_SESSION[ 'login' ], '', '', '', '', '', '', '','');
+		$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ], '' );
 		$ADMIN = $USUARIO->comprobarAdmin();
-			if($ADMIN == true){
-				if ( !$_POST ) {//Si no se han recibido datos 
-			$FUNCIONALIDAD = new FUNCIONALIDAD( '', '', '', '');
-		//Si se reciben datos
+		if ( $ADMIN == true ) {
+			if ( !$_POST ) { //Si no se han recibido datos 
+				$FUNCIONALIDAD = new FUNCIONALIDAD( '', '', '' );
+				//Si se reciben datos
+			} else {
+				$FUNCIONALIDAD = get_data_form();
+			}
+			//Variable que almacena los datos de la busqueda
+			$datos = $FUNCIONALIDAD->SEARCH();
+			//Variable que almacena array con el nombre de los atributos
+			$lista = array( 'IdFuncionalidad', 'NombreFuncionalidad', 'DescripFuncionalidad' );
+			$PERMISO = $USUARIO->comprobarPermisos();
+			//Creacion de la vista showall con el array $lista, los datos y la ruta de vuelta
+			new FUNCIONALIDAD_SHOWALL( $lista, $datos, $PERMISO, true );
 		} else {
-			$FUNCIONALIDAD = get_data_form();
-		}
-		//Variable que almacena los datos de la busqueda
-		$datos = $FUNCIONALIDAD->SEARCH();
-		//Variable que almacena array con el nombre de los atributos
-		$lista = array( 'IdFuncionalidad','NombreFuncionalidad','DescripFuncionalidad' );
-		$PERMISO = $USUARIO->comprobarPermisos();
-		//Creacion de la vista showall con el array $lista, los datos y la ruta de vuelta
-		new FUNCIONALIDAD_SHOWALL( $lista, $datos,$PERMISO,true);
-			}else{
-			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ],'');
-            $cont=0;
+			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ], '' );
+			$cont = 0;
 			$PERMISO = $USUARIO->comprobarPermisos();
 			while ( $fila = mysqli_fetch_array( $PERMISO ) ) {
-			if($fila['IdFuncionalidad']=='3'){
-				if($fila['IdAccion']=='5'){
-			    //Crea una vista add para ver la tupla
-			     $cont=$cont+1;
+				if ( $fila[ 'IdFuncionalidad' ] == '3' ) {
+					if ( $fila[ 'IdAccion' ] == '5' ) {
+						//Crea una vista add para ver la tupla
+						$cont = $cont + 1;
+					}
 				}
-			   }
 			}
-			if($cont==1){
-		if ( !$_POST ) {//Si no se han recibido datos 
-			$FUNCIONALIDAD = new FUNCIONALIDAD( '', '', '','');
-		//Si se reciben datos
-		} else {
-			$FUNCIONALIDAD = get_data_form();
-		}
-		//Variable que almacena los datos de la busqueda
-		$datos = $FUNCIONALIDAD->SEARCH();
-		//Variable que almacena array con el nombre de los atributos
-		$lista = array( 'IdFuncionalidad','NombreFuncionalidad','DescripFuncionalidad' );
-		$PERMISO = $USUARIO->comprobarPermisos();
-		//Creacion de la vista showall con el array $lista, los datos y la ruta de vuelta
-		new FUNCIONALIDAD_SHOWALL( $lista, $datos,$PERMISO,false);
-		}else{
-		 new USUARIO_DEFAULT();
-		}
+			if ( $cont == 1 ) {
+				if ( !$_POST ) { //Si no se han recibido datos 
+					$FUNCIONALIDAD = new FUNCIONALIDAD( '', '', '' );
+					//Si se reciben datos
+				} else {
+					$FUNCIONALIDAD = get_data_form();
+				}
+				//Variable que almacena los datos de la busqueda
+				$datos = $FUNCIONALIDAD->SEARCH();
+				//Variable que almacena array con el nombre de los atributos
+				$lista = array( 'IdFuncionalidad', 'NombreFuncionalidad', 'DescripFuncionalidad' );
+				$PERMISO = $USUARIO->comprobarPermisos();
+				//Creacion de la vista showall con el array $lista, los datos y la ruta de vuelta
+				new FUNCIONALIDAD_SHOWALL( $lista, $datos, $PERMISO, false );
+			} else {
+				new USUARIO_DEFAULT();
 			}
+		}
 }
 
 ?>
--- Models/FUNCIONALIDAD_MODEL.php
+++ Models/FUNCIONALIDAD_MODEL.php
@@ -3,33 +3,32 @@
 //Modelo que interactúa con el controlador de FUNCIONALIDAD y la tabla de FUNCIONALIDAD de la base de datos.
 //Fecha de creación:26/11/2017
 
-    class FUNCIONALIDAD{
-       
+class FUNCIONALIDAD {
 
-        var $IdFuncionalidad;//Declaración de la variable IdFuncionalidad, es la clave.
-        var $NombreFuncionalidad;//Declaración de la variable NombreFuncionalidad
-        var $DescripFuncionalidad;//Declaración de la variable DescripFuncionalidad
-        var $dependencias;//Declaración de la variable dependencias
-        
-        
-        
-            function __construct($IdFuncionalidad,$NombreFuncionalidad,$DescripFuncionalidad,$Acciones){
-                //Asignamos valores a los atibutos de la clase.
-                $this->IdFuncionalidad=$IdFuncionalidad;
-                $this->NombreFuncionalidad=$NombreFuncionalidad;
-                $this->DescripFuncionalidad=$DescripFuncionalidad;
-				$this->Acciones=$Acciones;
-                
-                 // incluimos la funcion de acceso a la bd
-		              include_once '../Functions/BdAdmin.php';
-		        // conectamos con la bd y guardamos el manejador en un atributo de la clase
-		              $this->mysqli = ConectarBD();
-            }//fin del constructor.
-        
-        
-        //funcion SEARCH: hace una búsqueda en la tabla con
-	   //los datos proporcionados. Si van vacios devuelve todos
-	   function SEARCH() {
+
+	var $IdFuncionalidad; //Declaración de la variable IdFuncionalidad, es la clave.
+	var $NombreFuncionalidad; //Declaración de la variable NombreFuncionalidad
+	var $DescripFuncionalidad; //Declaración de la variable DescripFuncionalidad
+	var $dependencias; //Declaración de la variable dependencias
+
+
+
+	function __construct( $IdFuncionalidad, $NombreFuncionalidad, $DescripFuncionalidad ) {
+		//Asignamos valores a los atibutos de la clase.
+		$this->IdFuncionalidad = $IdFuncionalidad;
+		$this->NombreFuncionalidad = $NombreFuncionalidad;
+		$this->DescripFuncionalidad = $DescripFuncionalidad;
+
+		// incluimos la funcion de acceso a la bd
+		include_once '../Functions/BdAdmin.php';
+		// conectamos con la bd y guardamos el manejador en un atributo de la clase
+		$this->mysqli = ConectarBD();
+	} //fin del constructor.
+
+
+	//funcion SEARCH: hace una búsqueda en la tabla con
+	//los datos proporcionados. Si van vacios devuelve todos
+	function SEARCH() {
 		// construimos la sentencia de busqueda con LIKE y los atributos de la entidad
 		$sql = "select IdFuncionalidad,
                         NombreFuncionalidad,
@@ -49,8 +48,8 @@
 			return $resultado;
 		}
 	} // fin metodo SEARCH
-        
-      //Metodo ADD()
+
+	//Metodo ADD()
 	//Inserta en la tabla  de la bd  los valores
 	// de los atributos del objeto. Comprueba si la clave esta vacia y si 
 	//existe ya en la tabla
@@ -63,8 +62,8 @@
 			if ( !$result = $this->mysqli->query( $sql ) ) { // si da error la ejecución de la query
 				return 'No se ha podido conectar con la base de datos'; // error en la consulta (no se ha podido conectar con la bd). Devolvemos un mensaje que el controlador manejara
 			} else { // si la ejecución de la query no da error
-                if ($result->num_rows == 0){ // miramos si el resultado de la consulta es vacio
-                    //hacemos la inserción en la base de datos
+				if ( $result->num_rows == 0 ) { // miramos si el resultado de la consulta es vacio
+					//hacemos la inserción en la base de datos
 					$sql = "INSERT INTO FUNCIONALIDAD (
 							    IdFuncionalidad,
                                 NombreFuncionalidad,
@@ -74,25 +73,24 @@
 								'$this->NombreFuncionalidad',
 								'$this->DescripFuncionalidad'
 								)";
-                }
-                    else{
-                        	return 'Ya existe esa funcionalidad  en la base de datos'; // ya existe
-                    }
-                }
-					if ( !$this->mysqli->query( $sql ) ) { // si da error en la ejecución del insert devolvemos mensaje
-						return 'Error en la inserción';
-					} else { //si no da error en la insercion devolvemos mensaje de exito
-						return 'Inserción realizada con éxito'; //operacion de insertado correcta
-					}
+				} else {
+					return 'Ya existe esa funcionalidad  en la base de datos'; // ya existe
+				}
+			}
+			if ( !$this->mysqli->query( $sql ) ) { // si da error en la ejecución del insert devolvemos mensaje
+				return 'Error en la inserción';
+			} else { //si no da error en la insercion devolvemos mensaje de exito
+				return 'Inserción realizada con éxito'; //operacion de insertado correcta
+			}
 
-				} else // si ya existe ese valor de clave en la tabla devolvemos el mensaje correspondiente
-					return 'Inserta un valor'; // ya existe
-    
+		} else // si ya existe ese valor de clave en la tabla devolvemos el mensaje correspondiente
+			return 'Inserta un valor'; // ya existe
+
 	} // fin del metodo ADD   
-      
-        // funcion DELETE()
-	    // comprueba que exista el valor de clave por el que se va a borrar,si existe se ejecuta el borrado, sino
-	    // se manda un mensaje de que ese valor de clave no existe
+
+	// funcion DELETE()
+	// comprueba que exista el valor de clave por el que se va a borrar,si existe se ejecuta el borrado, sino
+	// se manda un mensaje de que ese valor de clave no existe
 	function DELETE() {
 		// se construye la sentencia sql de busqueda con los atributos de la clase
 		$sql = "SELECT * FROM FUNCIONALIDAD WHERE (IdFuncionalidad = '$this->IdFuncionalidad')";
@@ -104,7 +102,7 @@
 			// se construye la sentencia sql de borrado
 			$sql = "DELETE FROM FUNCIONALIDAD WHERE (IdFuncionalidad = '$this->IdFuncionalidad' )";
 			$sql2 = "DELETE FROM FUNC_ACCION WHERE (IdFuncionalidad = '$this->IdFuncionalidad' )";
-			$sql3= "DELETE FROM PERMISO WHERE (IdFuncionalidad = '$this->IdFuncionalidad' )";
+			$sql3 = "DELETE FROM PERMISO WHERE (IdFuncionalidad = '$this->IdFuncionalidad' )";
 			// se ejecuta la query
 			$this->mysqli->query( $sql );
 			$this->mysqli->query( $sql2 );
@@ -115,11 +113,11 @@
 		else
 			return "No existe";
 	} // fin metodo DELETE
-        
-        
-        // funcion RellenaDatos()
-        // Esta función obtiene de la entidad de la bd todos los atributos a partir del valor de la clave que esta
-	   // en el atributo de la clase
+
+
+	// funcion RellenaDatos()
+	// Esta función obtiene de la entidad de la bd todos los atributos a partir del valor de la clave que esta
+	// en el atributo de la clase
 	function RellenaDatos() { // se construye la sentencia de busqueda de la tupla
 
 		$sql = "SELECT * FROM FUNCIONALIDAD WHERE (IdFuncionalidad = '$this->IdFuncionalidad')";
@@ -131,37 +129,37 @@
 			return $result;
 		}
 	} // fin del metodo RellenaDatos()
-        
-        // funcion RellenaDatos()
+
+	// funcion RellenaDatos()
 	// Esta función obtiene de la entidad de la bd todos los atributos a partir del valor de la clave que esta
 	// en el atributo de la clase
 	function dependencias() { // se construye la sentencia de busqueda de la tupla
-        
-        $dependencias = null;
+
+		$dependencias = null;
 
 		$sql = "SELECT * FROM FUNC_ACCION WHERE (IdFuncionalidad = '$this->IdFuncionalidad')";
-        $resultado = $this->mysqli->query( $sql );
-        if ( $resultado->num_rows == 1 ) {
-            $result = $resultado->fetch_array();
-            $keys = array('FUNC_ACCION');
-            $dependencias = array_fill_keys($keys , $result);
-        }
-        
-        
-        
+		$resultado = $this->mysqli->query( $sql );
+		if ( $resultado->num_rows == 1 ) {
+			$result = $resultado->fetch_array();
+			$keys = array( 'FUNC_ACCION' );
+			$dependencias = array_fill_keys( $keys, $result );
+		}
+
+
+
 		$sql = "SELECT * FROM PERMISO WHERE (IdFuncionalidad = '$this->IdFuncionalidad')";
-        $resultado = $this->mysqli->query( $sql );
-        if ( $resultado->num_rows == 1 ) {
-            $result = $resultado->fetch_array();
-            $keys = array('PERMISO');
-            $dependencias = array_fill_keys($keys , $result);
-        }
-        
-        return $dependencias;
+		$resultado = $this->mysqli->query( $sql );
+		if ( $resultado->num_rows == 1 ) {
+			$result = $resultado->fetch_array();
+			$keys = array( 'PERMISO' );
+			$dependencias = array_fill_keys( $keys, $result );
+		}
+
+		return $dependencias;
 	} // fin del metodo RellenaDatos()
-		
-		
-    function RellenaSelect() { // se construye la sentencia de busqueda de la tupla
+
+
+	function RellenaSelect() { // se construye la sentencia de busqueda de la tupla
 
 		$sql = "SELECT * FROM ACCION";
 		// Si la busqueda no da resultados, se devuelve el mensaje de que no existe
@@ -171,21 +169,20 @@
 			return $resultado;
 		}
 	} // fin del metodo RellenaSelect()	
-         // funcion EDIT()
-	    // Se comprueba que la tupla a modificar exista en base al valor de su clave primaria
-	   // si existe se modifica
-		function EDIT() {
+	// funcion EDIT()
+	// Se comprueba que la tupla a modificar exista en base al valor de su clave primaria
+	// si existe se modifica
+	function EDIT() {
 		// se construye la sentencia de busqueda de la tupla en la bd
 		$sql = "SELECT * FROM FUNCIONALIDAD WHERE (idFuncionalidad = '$this->IdFuncionalidad')";
-		$Acc = $this->Acciones;
 		// se ejecuta la query
 		$result = $this->mysqli->query( $sql );
 		// si el numero de filas es igual a uno es que lo encuentra
 		if ( $result->num_rows == 1 ) {
 			// se construye la sentencia de modificacion en base a los atributos de la clase
-			if ( count($Acc) == 0){
-			     //modificamos los atributos de la tabla USUARIO
-				$sql = "UPDATE FUNCIONALIDAD SET 
+
+			//modificamos los atributos de la tabla FUNCIONALIDAD
+			$sql = "UPDATE FUNCIONALIDAD SET 
 					IdFuncionalidad = '$this->IdFuncionalidad',
                     NombreFuncionalidad='$this->NombreFuncionalidad',
 					DescripFuncionalidad = '$this->DescripFuncionalidad'
@@ -198,38 +195,16 @@
 				return 'Modificado correctamente';
 			}
 
-		} else{
-				$cont=0;	  
-				for ($i=0;$i<count($Acc);$i++){
-				 $sqli[$i] = "INSERT INTO FUNC_ACCION
-										(IdFuncionalidad,IdAccion)
-										VALUES
-										('$this->IdFuncionalidad',
-										 '$Acc[$i]'
-										)";
-				if ( !( $resultado = $this->mysqli->query( $sqli[$i] ) ) ) {
-				
-			     }else{
-				$cont=$cont+1;
-				}	
-				}
-								// si hay un problema con la query se envia un mensaje de error en la modificacion
-			if ( $cont!= (count($Acc))) {
-				return 'Error en la modificación';
-			} else { // si no hay problemas con la modificación se indica que se ha modificado
-				return 'Modificado correctamente';
-			}
-			}
 		} // si no se encuentra la tupla se manda el mensaje de que no existe la tupla
-		            else
-				return 'No existe en la base de datos';		
+		else
+			return 'No existe en la base de datos';
 	} // fin del metodo EDIT
 
 
-        
-                      
-        
-    }
+
+
+
+}
 
 
 ?>
