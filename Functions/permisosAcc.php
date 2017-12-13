b1c938bb3e0ad41e35d4f7908e519414fb8a544d
AlexVC14 <alexvolocid@gmail.com>
2017-12-13 16:37:09 +0100

    Modificada funcion ACL

--- Functions/permisosAcc.php
+++ Functions/permisosAcc.php
@@ -9,13 +9,18 @@
 include_once '../Functions/BdAdmin.php';
 function permisosAcc($login,$funcionalidad,$accion){
 	   $mysqli=ConectarBD();
-	   $sql = "SELECT DISTINCT U.login, P.IdGrupo, P.IdFuncionalidad FROM PERMISO P, USU_GRUPO U WHERE U.login = '$login' && (U.IdGrupo = '00000A' || (U.IdGrupo = P.IdGrupo && P.IdFuncionalidad = '$funcionalidad' && P.IdAccion = '$accion') )";
+	   $sql="SELECT DISTINCT login FROM USU_GRUPO WHERE login='$login' && IdGrupo='00000A' ";
 	   $resultado = $mysqli->query( $sql );
-		if($resultado->num_rows == 0){//miramos si el numero de filas es 0
+	if($resultado->num_rows == 0){
+	   $sql1 = "SELECT DISTINCT U.login, P.IdGrupo, P.IdFuncionalidad FROM PERMISO P, USU_GRUPO U WHERE U.login = '$login' && (U.IdGrupo = '00000A' || (U.IdGrupo = P.IdGrupo && P.IdFuncionalidad = '$funcionalidad' && P.IdAccion = '$accion') )";
+	   $resultado1 = $mysqli->query( $sql1 );
+		if($resultado1->num_rows == 0){//miramos si el numero de filas es 0
 			return false;
 		}else{
 			return true;
 		}
-
+	}else{
+		return true;
+	}
 } //end of function permisosFun()
 ?>
