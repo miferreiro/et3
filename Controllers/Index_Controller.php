21560e4a71f57aec07875db9cb2095f8716b20c5
Brais <santosbrais@gmail.com>
2017-12-16 23:31:18 +0100

    mas comentarios

--- Controllers/Index_Controller.php
+++ Controllers/Index_Controller.php
@@ -2,7 +2,7 @@
 /*
 	Archivo php
 	Nombre: Index_Controller.php
-	Autor: 	fta875
+	Autor: 	Miguel Ferreiro
 	Fecha de creación: 23/10/2017 
 	Función: controlador que comprueba si el usuario está autenticado o no
 */
@@ -12,12 +12,12 @@ session_start();
 include '../Functions/Authentication.php';
 //si no esta autenticado
 if (!IsAuthenticated()){
-	header('Location: ../index.php');
+	header('Location: ../index.php');//redirige a la página de inicio
 }
 //esta autenticado
 else{
-	include '../Views/users_index_View.php';
-	new Index();
+	include '../Views/users_index_View.php';//incluimos esa vista
+	new Index();// se crea un objeto de tipo Index
 }
 
 ?>
--- Controllers/Registro_Controller.php
+++ Controllers/Registro_Controller.php
@@ -2,37 +2,37 @@
 /*
 	Archivo php
 	Nombre: Register_Controller.php
-	Autor: fta875
+	Autor: Miguel Ferreiro
 	Fecha de creación: 23/10/2017 
 	Función: controlador que realiza las operaciones necesarias para realizar un registro correcto de un nuevo usuario
 */
-session_start();
-include_once '../Locales/Strings_'.$_SESSION['idioma'].'.php';
+session_start();//se inicia la sesión
+include_once '../Locales/Strings_'.$_SESSION['idioma'].'.php';//incluimos los strings para poder cambiar de idioma(español,galego,inglés)
 
-if(!isset($_POST['login'])){
-	include '../Views/Register_View.php';
-	$register = new Register();
+if(!isset($_POST['login'])){//Si no se han recibido datos del login
+	include '../Views/Register_View.php';//incluimos la vista para Registrarse
+	$register = new Register();//creamos un objeto de tipo Registro para registrarnos
 }
 else{
 		
-	include '../Models/USUARIO_MODEL.php';
+	include '../Models/USUARIO_MODEL.php';//incluye el contenido del modelo USUARIO_MODEL
 	//Variable que almacena un objecto de usuarios model
-	$usuario = new USUARIO_MODEL($_REQUEST['login'],$_REQUEST['password'],$_REQUEST['DNI'],$_REQUEST['nombre'],$_REQUEST['apellidos'],$_REQUEST['email'],$_REQUEST['direc'],$_REQUEST['telefono'],'');
+	$usuario = new USUARIO_MODEL($_REQUEST['login'],$_REQUEST['password'],$_REQUEST['DNI'],$_REQUEST['nombre'],$_REQUEST['apellidos'],$_REQUEST['email'],$_REQUEST['direc'],$_REQUEST['telefono'],'');//creamos un objeto del modelo USUARIO_MODEL donde le pasamos todos los datos del usuario registrado
 	//Almacena la respuesta de si esta existe el login o no para poder registrarse
 	$respuesta = $usuario->Register();
 
 	//Si no existe el login en la base de datos
 	if ($respuesta == 'true'){
-			$respuesta = $usuario->ADD();
+			$respuesta = $usuario->ADD();//añadimos este usuario a la base de datos
 		//Incluye la vista mensaje
-		include '../Views/MESSAGE_View.php';
-		new MESSAGE($respuesta, '../index.php');
+		include '../Views/MESSAGE_View.php';//incluimos la vista para que nos mandé un mensaje por pantalla
+		new MESSAGE($respuesta, '../index.php');//creamos un objeto de tipo mensaje que mostrara el mensaje que viene del modelo.
 	}
 	//Si existe en la base de datos
 	else{
 		//Incluye la vista mensaje
-		include '../Views/MESSAGE_View.php';
-		new MESSAGE($respuesta, '../index.php');
+		include '../Views/MESSAGE_View.php';//incluimos la vista para que nos mandé un mensaje por pantalla
+		new MESSAGE($respuesta, '../index.php');//creamos un objeto de tipo mensaje que mostrara el mensaje que viene del modelo.
 	}
 
 }
