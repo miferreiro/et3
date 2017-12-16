c3d031171f22cc1c932cdd575bfbf62f3bdc2252
Brais <santosbrais@gmail.com>
2017-12-17 00:06:22 +0100

    controlador de usuarios corregido pequeño fallo

--- Controllers/USUARIO_CONTROLLER.php
+++ Controllers/USUARIO_CONTROLLER.php
@@ -2,8 +2,8 @@
 /*
 	Archivo php
 	Nombre: USUARIO_CONTROLLER.php
-	Autor: 	fta875
-	Fecha de creación: 9/10/2017 
+	Autor: 	Jonatan Couto
+	Fecha de creación: 18/11/2017 
 	Función: controlador que realiza las acciones, recibidas de las vistas, necesarias para realizar altas, bajas, modificaciones y búsquedas.
 */
 session_start(); //solicito trabajar con la session
@@ -54,28 +54,28 @@ if ( !isset( $_REQUEST[ 'action' ] ) ) {
 switch ( $_REQUEST[ 'action' ] ) {
 	case 'ADD'://Caso añadir
 		if ( !$_POST ) {//Si no se han recibido datos se envia a la vista del formulario ADD
-			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ],'');
-			$ADMIN = $USUARIO->comprobarAdmin();
-			if($ADMIN == true){
+			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ],'');//creamos un objeto del modelo USU_GRUPO pasandole el usuario que está conectado
+			$ADMIN = $USUARIO->comprobarAdmin();//miramos si este usuario es administrador
+			if($ADMIN == true){//si el usuario es administrador mostramos la vista ADD
 				new USUARIO_ADD();
-			}else{
-            $cont=0;
-			$PERMISO = $USUARIO->comprobarPermisos();
-			while ( $fila = mysqli_fetch_array( $PERMISO ) ) {
-			if($fila['IdFuncionalidad']=='1'){
-				if($fila['IdAccion']=='0'){
+			}else{//si no es administrador
+            $cont=0;//iniciamos la variable a 0
+			$PERMISO = $USUARIO->comprobarPermisos();//llamamos a la función comprobarPermisos para saber los permisos que tiene el usuario
+			while ( $fila = mysqli_fetch_array( $PERMISO ) ) {//este bucle se va repetir mientras haya permisos
+			if($fila['IdFuncionalidad']=='1'){//miramos si este usuario tiene la funcionalidad de Gestión de usuarios
+				if($fila['IdAccion']=='0'){//miramos si este usuario tiene la acción de añadir
 			    //Crea una vista add para ver la tupla
-			     $cont=$cont+1;
+			     $cont=$cont+1;//si se cumple estos if incrementamos el contador a uno
 				}
 			   } 
 			}
-			if($cont==1){
+			if($cont==1){//miramos si la variable contador es 1, si es así mostramos la vista ADD
 			new USUARIO_ADD();
-			}else{
+			}else{//si no es igual a unose muestra un mensaje de que este usuario no tiene dichos permisos
 			new MESSAGE( 'El usuario no tiene los permisos necesarios', '../Controllers/USUARIO_CONTROLLER.php' );
 			}
 			}
-		} else {//Si recive datos los recoge y mediante las funcionalidad de USUARIO_MODEL inserta los datos
+		} else {//Si recibe datos los recoge y mediante las funcionalidad de USUARIO_MODEL inserta los datos
 			$USUARIO = get_data_form();//Variable que almacena los datos recogidos
 			$respuesta = $USUARIO->ADD();//Variable que almacena la respuesta de la inserción
 			//Crea la vista con la respuesta y la ruta para volver
@@ -85,14 +85,15 @@ switch ( $_REQUEST[ 'action' ] ) {
 		break;
 	case 'DELETE'://Caso borrar
 		if ( !$_POST ) {//Si no se han recibido datos se envia a la vista del formulario DELETE
-			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ],'');
-			$ADMIN = $USUARIO->comprobarAdmin();
-			if($ADMIN == true){
+			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ],'');//creamos un objeto del modelo USU_GRUPO pasandole el usuario que está conectado
+			$ADMIN = $USUARIO->comprobarAdmin();//llamando a esta función podremos saber si este usuario es administrador
+			if($ADMIN == true){//miramos si este usuario es administrador
 			//Variable que recoge un objecto model con solo el login
 			$USUARIO = new USUARIO_MODEL( $_REQUEST[ 'login' ], '', '', '', '', '', '', '');
 			//Variable que almacena el relleno de los datos utilizando el login
 			$valores = $USUARIO->RellenaDatos( $_REQUEST[ 'login' ] );
 			
+            //mostramos todas las dependencias que tiene  tabla USUARIOS a la hora de realizar el borrado
             $dependencias = $USUARIO->dependencias($_REQUEST['login']);
             $dependencias2 = $USUARIO->dependencias2($_REQUEST['login']);
             $dependencias3 = $USUARIO->dependencias3($_REQUEST['login']);
@@ -102,23 +103,24 @@ switch ( $_REQUEST[ 'action' ] ) {
                 
             //Crea una vista delete para ver la tupla
 			new USUARIO_DELETE( $valores,$dependencias, $dependencias2, $dependencias3, $dependencias4, $dependencias5, $dependencias6 );
-			}else{
-			$cont=0;
-			$PERMISO = $USUARIO->comprobarPermisos();
-						while ( $fila = mysqli_fetch_array( $PERMISO ) ) {
-			if($fila['IdFuncionalidad']=='1'){
-				if($fila['IdAccion']=='1'){
-			    //Crea una vista add para ver la tupla
-			     $cont=$cont+1;
+			}else{//si el usuario no es administrador
+			$cont=0;//inicializamos la variable cont a 0.
+			$PERMISO = $USUARIO->comprobarPermisos();//llamamos a esta función para comprobar los permisos que tiene dicho usuario
+						while ( $fila = mysqli_fetch_array( $PERMISO ) ) {//este bucle se repite mientras haya permisos
+			if($fila['IdFuncionalidad']=='1'){//miramos si este usuario tiene la funcionalidad de Gestión de Usuarios
+				if($fila['IdAccion']=='1'){//miramos si el usuario tiene la acción para borrar
+			   
+			     $cont=$cont+1;//incrementamos la variable cont a uno
 				}
 			  }
 			}
-			if($cont==1){
+			if($cont==1){//si la variable cont es igual a uno
 			//Variable que recoge un objecto model con solo el login
 			$USUARIO = new USUARIO_MODEL( $_REQUEST[ 'login' ], '', '', '', '', '', '', '');
 			//Variable que almacena el relleno de los datos utilizando el login
 			$valores = $USUARIO->RellenaDatos( $_REQUEST[ 'login' ] );
 			
+             //mostramos todas las dependencias que tiene  tabla USUARIOS a la hora de realizar el borrado   
             $dependencias = $USUARIO->dependencias($_REQUEST['login']);
             $dependencias2 = $USUARIO->dependencias2($_REQUEST['login']);
             $dependencias3 = $USUARIO->dependencias3($_REQUEST['login']);
@@ -127,12 +129,12 @@ switch ( $_REQUEST[ 'action' ] ) {
             $dependencias6 = $USUARIO->dependencias6($_REQUEST['login']);
             //Crea una vista delete para ver la tupla
 			new USUARIO_DELETE( $dependencias, $dependencias2, $dependencias3, $dependencias4, $dependencias5, $dependencias6 );
-			}else{
+			}else{//si la variable cont no es uno mostramos un mensaje diciendo que dicho usuario no tiene permiso
 			new MESSAGE( 'El usuario no tiene los permisos necesarios', '../Controllers/USUARIO_CONTROLLER.php' );
 			}
 			}
 			//Si recibe valores ejecuta el borrado
-		} else {
+		} else {//Si recibe datos los recoge y mediante las funcionalidad de USUARIO_MODEL borra los datos
 			//Variable que almacena los datos recogidos de los atributos
 			$USUARIO = get_data_form();
 			//Variable que almacena la respuesta de realizar el borrado
@@ -144,13 +146,17 @@ switch ( $_REQUEST[ 'action' ] ) {
 		break;
 	case 'EDIT'://Caso editar	
 		if ( !$_POST ) {//Si no se han recibido datos se envia a la vista del formulario EDIT
-			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ],'');
-			$ADMIN = $USUARIO->comprobarAdmin();
-			if($ADMIN == true){
+			$USUARIO = new USU_GRUPO( $_SESSION[ 'login' ],'');//creamos un objeto del modelo USU_GRUPO pasandole el usuario que está conectado
+			$ADMIN = $USUARIO->comprobarAdmin();//comprobamos si dicho usuario es administrador
+			if($ADMIN == true){//si es el usuario es administrador
 						//Variable que almacena un objeto model con el login
 			$USUARIO = new USUARIO_MODEL( $_REQUEST[ 'login' ], '', '', '', '', '', '', '');
 			//Variable que almacena los datos de los atibutos rellenados a traves de login
 			$valores = $USUARIO->RellenaDatos( $_REQUEST[ 'login' ] );
+
+			$datos = $USUARIO->RellenaSelect();
+             
+
 			//Muestra la vista del formulario editar
 			new USUARIO_EDIT( $valores);
 			}else{
